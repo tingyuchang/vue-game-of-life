746e86ddc910fc408d37f00f8215b4243d1bc248
tingyuchang <tingyugenius@gmail.com>
2021-12-12 21:55:33 +0800

    import vue grid view

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world
+# Game-of-life
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="container">
-        <CellList :cells="cells" @cellSelect="onCellSelect"/>
+        <!-- <CellList :cells="cells" @cellSelect="onCellSelect"/> -->
+          <grid-layout 
+            :layout.sync="layout"
+            :col-num="20"
+            :row-height="30"
+            :is-draggable="false"
+            :is-resizable="true"
+            :is-mirrored="false"
+            :vertical-compact="false"
+            :use-css-transforms="false"
+            :useStyleCursor="false"
+        >
+            <grid-item class="cell" v-for="cell in cells"
+                       :key="cell.id"
+                       :x="cell.y"
+                       :y="cell.x"
+                       :w="1"
+                       :h="1"
+                       :i="cell.id"
+                       v-bind:class="{ live: cell.is_live }"
+            >
+            
+            </grid-item>
+        </grid-layout>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import CellList from './components/CellList'
+// import CellList from './components/CellList'
+import VueGridLayout from 'vue-grid-layout';
 
 export default {
   name: 'App',
   components: {
-        CellList: CellList,
+        // CellList: CellList,
+        GridLayout: VueGridLayout.GridLayout,
+        GridItem: VueGridLayout.GridItem
     },
   data () {
         return { 
             cells: [],
             selectedCell: null,
+            layout: [],
         };
     },
   methods: {
@@ -34,11 +61,37 @@ export default {
         }
   },
   mounted(){
-    this.onOpen()
+    this.$nextTick(function () {
+            window.setInterval(() => {
+                this.onOpen();
+            },1000);
+        })
  },
  
 }
 </script>
 
 <style scoped>
+  .container .vue-grid-item.vue-grid-placeholder {
+    background: green;
+}
+.vue-grid-layout {
+    background: #eee;
+}
+.vue-grid-item:not(.vue-grid-placeholder) {
+    border: 1px solid black;
+}
+.vue-grid-item .resizing {
+    opacity: 0.9;
+}
+.vue-grid-item .static {
+    background: #cce;
+}
+
+  .cell {
+    background-color: white;
+  }
+  .live{
+    background-color: black;
+  }
 </style>
